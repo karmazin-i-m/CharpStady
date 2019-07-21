165dd65731d50b8b68f9d8e3fc644408ba01c1c8
karmazin.i.m <karmazin.i.m@gmail.com>
2019-07-22 00:22:37 +0300

    Add ITVDN_Zad_Dop

---
+++ ITVDN_Zad_Dop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITVDN_Zad_Dop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свою дату рождения: ");
+            DateTime bithday = DateTime.Parse(Console.ReadLine());
+            DateTime now = DateTime.Now;
+
+            if (now.DayOfYear <= bithday.DayOfYear)
+            {
+                Console.WriteLine($"До вашего дня рождения еще {bithday.DayOfYear - now.DayOfYear}");
+            }
+            else
+                Console.WriteLine($"До вашего дня рождения еще {bithday.DayOfYear + 365 - now.DayOfYear} дня");
+
+            Console.ReadKey();
+        }
+    }
+}
