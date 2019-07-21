ad6e549dda94a7bf820a88c690c8cdeeb70f8ce1
karmazin.i.m <karmazin.i.m@gmail.com>
2019-07-21 14:31:58 +0300

    Add ClassWork_Zad_3

---
+++ ClassWork_Zad_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassWork_Zad_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2019, 7, 20, 13, 34, 0);
+
+            while (true)
+            {
+                Console.Write("Please write name of the fruit, that you eat or different operation: ");
+                string fruit = Console.ReadLine();
+
+                if ((Fruits.Apple.ToString() == fruit || Fruits.apple.ToString() == fruit) && date.DayOfWeek == DayOfWeek.Saturday && date.Hour >= 12)
+                {
+                    Console.WriteLine("Watch out you can die!");
+                }
+                else Console.WriteLine("Don't worry all is well!");
+            }
+        }
+    }
+    enum Fruits
+    {
+        Default,
+        Babana,
+        banana,
+        Apple,
+        apple,
+        Melon,
+        melon,
+        Watermelon,
+        watermelon,
+        Strawberry,
+        strawberry,
+    }
+}
