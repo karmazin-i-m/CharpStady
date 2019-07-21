d1bc9fc68658b4681054beb28c0597a3a93906f3
karmazin.i.m <karmazin.i.m@gmail.com>
2019-07-22 00:04:48 +0300

    Add ITVDN_Zad_2 and ITVDN_Zad_3

---
+++ ITVDN_Zad_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITVDN_Zad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер цвета: ");
+            int color = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите строку для вывода: ");
+            Priter.Print(Console.ReadLine(),color);
+
+            Console.ReadKey();
+        }
+    }
+
+    static class Priter
+    {
+        public static void Print(string @string, int color)
+        {
+            if (color <0 || color > 15)
+            {
+                Console.WriteLine("Такого цвета не существует");
+                return;
+            }
+            Console.ForegroundColor = (ConsoleColor)color;
+            Console.WriteLine(@string);
+
+        }
+    }
+}
---
+++ ITVDN_Zad_3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITVDN_Zad_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Accauntant accauntant = new Accauntant();
+            var array = (int[])Enum.GetValues(typeof(Workers));
+            Workers worker;
+            int hours = 120;
+            for (int i = 0; i < Enum.GetNames(typeof(Workers)).Length; i++)
+            {
+                worker = (Workers)array[i];
+                Console.WriteLine($"Премия для сотрудника: {worker} = {accauntant.AskForBonus(worker, hours)} ");
+                hours += 10;
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Accauntant
+    {
+        public bool AskForBonus(Workers worker, int hours)
+        {
+            if ((int)worker >= hours)
+            {
+                Console.WriteLine("Сотруднику положена премия");
+                return true;
+            }
+            Console.WriteLine("Сотруднику не положена премия");
+            return false;
+        }
+    }
+
+    enum Workers
+    {
+        Boss = 120,
+        Worker1 = 160,
+        Worker2 = 130,
+        Worker3 = 80,
+        Worker4 = 200,
+    }
+}
