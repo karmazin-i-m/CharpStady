3fa35e57ec517d2fb441a8b28b3d8c459e95f0b5
karmazin.i.m <karmazin.i.m@gmail.com>
2019-07-21 11:39:18 +0300

    Add ClassWork_Zad_1

---
+++ ClassWork_Zad_1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassWork_Zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите свою профессию: ");
+                string prof = Console.ReadLine();
+                Profession profession;
+
+                switch (prof)
+                {
+                    case "Программист":
+                        profession = Profession.Programmer;
+                        break;
+                    case "Шахтер":
+                        profession = Profession.Miner;
+                        break;
+                    case "Повар":
+                        profession = Profession.Cook;
+                        break;
+                    case "Бухгалтер":
+                        profession = Profession.Accountant;
+                        break;
+                    case "Босс":
+                        profession = Profession.Boss;
+                        break;
+                    case "q":
+                        Console.WriteLine("Выхожу из программы...");
+                        Thread.Sleep(2000);
+                        return;
+                    default:
+                        profession = Profession.Default;
+                        break;
+                }
+
+                if (profession != Profession.Default)
+                {
+                    Console.WriteLine("Ваша профессия есть в списке!");
+                }
+                else Console.WriteLine("Такой профессии нет в списке!");
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+    enum Profession : int
+    {
+        Default = 0,
+        Programmer,
+        Cook,
+        Miner,
+        Accountant,
+        Boss,
+    }
+}
