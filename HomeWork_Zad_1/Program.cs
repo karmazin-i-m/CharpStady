fcd5758d5f6d54f739d7f47152791080d0607142
karmazin.i.m <karmazin.i.m@gmail.com>
2019-07-22 14:57:05 +0300

    Add HomeWork_Zad_1

---
+++ HomeWork_Zad_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_Zad_1
+{
+    class Program
+    {
+        delegate int Calculate(int x, int y);
+        static void Main(string[] args)
+        {
+            Calculate add = (x, y) => x + y;
+            Calculate sub = (x, y) => x - y;
+            Calculate mul = (x, y) => x * y;
+            Calculate dif = (x, y) => x / y;
+
+            while (true)
+            {
+                Console.Write("Введите оператор: ");
+                if (!Char.TryParse(Console.ReadLine(), out char operand)) continue;
+                Console.Write("Введите х: ");
+                if (!Int32.TryParse(Console.ReadLine(), out int x)) continue;
+                Console.Write("Введите у: ");
+                if (!Int32.TryParse(Console.ReadLine(), out int y)) continue;
+
+                switch (operand)
+                {
+                    case '+':
+                        Console.WriteLine(add(x,y));
+                        break;
+                    case '-':
+                        Console.WriteLine(sub(x, y));
+                        break;
+                    case '/':
+                        Console.WriteLine(dif(x, y));
+                        break;
+                    case '*':
+                        Console.WriteLine(mul(x, y));
+                        break;
+                    case 'q':
+                        Console.WriteLine("Exit...");
+                        System.Threading.Thread.Sleep(2000);
+                        return;
+                    default:
+                        Console.WriteLine("Я не знаю такого оператора :(");
+                        break;
+                }
+            }
+
+        }
+    }
+}
