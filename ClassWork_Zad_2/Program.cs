a83e9925593ce10f234160a7c0084ef46c47f471
karmazin.i.m <karmazin.i.m@gmail.com>
2019-07-22 14:21:24 +0300

    Add ClassWork_Zad_2

---
+++ ClassWork_Zad_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork_Zad_2
+{
+    class Program
+    {
+        delegate int MyDelegate(int num);
+        static void Main(string[] args)
+        {
+            MyDelegate increment = Increment;
+            MyDelegate decrement = Decrement;
+
+            Console.Write("Введите число для операций: ");
+            int num = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(increment(num));
+            Console.WriteLine(decrement(num));
+
+            Console.ReadKey();
+        }
+
+        static int Increment(int num)
+        {
+            return ++num;
+        }
+
+        static int Decrement(int num)
+        {
+            return --num;
+        }
+    }
+}
