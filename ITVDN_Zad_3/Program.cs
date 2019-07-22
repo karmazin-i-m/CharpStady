b431f0d096d5b5d502ef879018033a0e6b6ca981
karmazin.i.m <karmazin.i.m@gmail.com>
2019-07-22 15:36:47 +0300

    Add ITVDN_Zad_3

---
+++ ITVDN_Zad_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITVDN_Zad_3
+{
+    class Program
+    {
+        delegate int RandomInt();
+        delegate int MidleRandomInt(RandomInt[] randoms);
+        static void Main(string[] args)
+        {
+            RandomInt randomer = delegate
+            {
+                Random random = new Random(DateTime.Now.Millisecond);
+                return random.Next(0,1000);
+            };
+
+            RandomInt[] randome = { randomer, randomer, randomer, randomer, randomer };
+
+            MidleRandomInt result = delegate (RandomInt[] randoms)
+            {
+                int res = 0;
+                for (int i = 0; i < randoms.Length; i++)
+                {
+                    res += randoms[i].Invoke();
+                }
+                res /= randoms.Length;
+                return res;
+            };
+
+            Console.WriteLine(result(randome));
+            Console.ReadKey();
+        }
+    }
+}
