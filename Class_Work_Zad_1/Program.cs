17d2b29068eb88ec35eca4fdb419de7f241cdfca
karmazin.i.m <karmazin.i.m@gmail.com>
2019-07-22 13:44:37 +0300

    Add Class_Zad_1

--- Class_Work_Zad_1/Program.cs
+++ Class_Work_Zad_1/Program.cs
@@ -8,8 +8,52 @@ namespace Class_Work_Zad_1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            string[] array =
+            {
+                "Моя кошка любит корм",
+                "Сколько гор во всем мире?",
+                "Монте-Карло город казино",
+                "Дорога в никуда",
+                "Моя мама печет пироги",
+                "Масик кличка моего кота"
+            };
+
+            Predicate predicate = delegate (string @string)
+            {
+                if (@string.ToArray()[0] == 'М')
+                {
+                    for (int j = 0; j < @string.Length; j++)
+                    {
+                        if (@string.ToArray()[j] == 'р' && @string.ToArray()[j - 1] == 'о')
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].Shows(predicate);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+    delegate bool Predicate(string str);
+    static class Extension
+    {
+        public static void Shows(this string str, Predicate predicate)
+        {
+            if (predicate(str))
+            {
+                Console.WriteLine(str);
+            }
         }
     }
 }
