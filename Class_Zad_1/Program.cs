2c52af36970224a01daeef9fbbbef6c14a943fc9
karmazin.i.m <karmazin.i.m@gmail.com>
2019-08-10 17:40:40 +0300

    Недоделанная работа, нужно наладить работу исключений, и метода Wait();

--- Class_Zad_1/Program.cs
+++ Class_Zad_1/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Class_Zad_1
 {
@@ -10,6 +10,41 @@ namespace Class_Zad_1
     {
         static void Main(string[] args)
         {
+
+            ThreadWorker<int> threadWorker = new ThreadWorker<int>(Count<int>);
+
+            threadWorker.Start();
+            System.Console.WriteLine(threadWorker.Result);
+
+            threadWorker.Start();
+            System.Threading.Thread.Sleep(1000);
+            System.Console.WriteLine(threadWorker.Result);
+
+            try
+            {
+                ThreadWorker<int> thread = new ThreadWorker<int>(() => {
+                    int res = 0;
+                    return 24/res;
+                    });
+                System.Console.WriteLine(thread.Result);
+            }catch(System.DivideByZeroException e)
+            {
+                System.Console.WriteLine("Попытка деления на нуль.");
+            }
+
+            System.Console.ReadKey();
+        }
+
+        
+
+        static int Count<Tres>()
+        {
+            int res = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                res += i;
+            }
+            return res;
         }
     }
 }
---
+++ Class_Zad_1/ThreadWorker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+namespace Class_Zad_1
+{
+    class ThreadWorker<TResult>
+    {
+        Func<TResult> funk;
+        TResult result;
+
+        public ThreadWorker(Func<TResult> funk)
+        {
+            this.funk = funk;
+        }
+        public bool IsCompleted { get; private set; } = false;
+        public bool IsSuccess { get; private set; } = false;
+        public Exception Exception { get; private set; } = null;
+
+        public TResult Result {
+            get
+            {
+                if (Exception == null)
+                {
+                    if (result != null)
+                        return result;
+                    else Wait();
+                    return result;
+                }
+                else throw Exception;
+            }
+        }
+
+        public void Start()
+        { 
+            try
+            {
+                Thread thread = new Thread(() => {
+                    result = funk.Invoke();
+                });
+                thread.Start();
+                IsSuccess = true;
+            }
+            catch (Exception e)
+            {
+                Exception = e;
+                IsSuccess = false;
+            }
+            finally
+            {
+                IsCompleted = true;
+            }
+            
+        }
+
+        private void Wait()
+        {
+            Console.WriteLine("Ваш результат еще не готов, немного подождите...");
+            Thread.Sleep(1000);
+        }
+    }
+}
