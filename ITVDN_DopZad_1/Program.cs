404df236bcfb91800fc60e8eda4c342486f00a6c
karmazin.i.m <karmazin.i.m@gmail.com>
2019-08-11 15:19:31 +0300

    Работа над заданием 1

---
+++ ITVDN_DopZad_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ITVDN_DopZad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main");
+            Metod1();
+        }
+
+        static void Metod1()
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            Thread thread = new Thread(Metod2);
+            thread.Start();
+        }
+        static void Metod2()
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            Thread thread = new Thread(Metod1);
+            thread.Start();
+        }
+    }
+}
---
+++ ITVDN_Zad_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITVDN_Zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print(5);
+
+            Console.ReadKey();
+        }
+
+        static void Print(int ColumNumber)
+        {
+            Random random = new Random((int)DateTime.Now.Ticks);
+
+            Console.CursorLeft = ColumNumber;
+            Console.Write("asfasf");
+        }
+    }
+}
