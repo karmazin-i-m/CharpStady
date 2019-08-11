9e3fd2fa447953ef20db8864a50db155bcb62189
karmazin.i.m <karmazin.i.m@gmail.com>
2019-08-11 17:17:28 +0300

    Доделать

--- ITVDN_Zad_1/Program.cs
+++ ITVDN_Zad_1/Program.cs
@@ -18,9 +18,27 @@ namespace ITVDN_Zad_1
         static void Print(int ColumNumber)
         {
             Random random = new Random((int)DateTime.Now.Ticks);
+            char[] array = new char[255];
 
-            Console.CursorLeft = ColumNumber;
-            Console.Write("asfasf");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = (char)random.Next(255);
+            }
+            array.Mix();
         }
+
+        static char[] Mix(char[] array)
+        {
+            Random random = new Random((int)DateTime.Now.Ticks);
+            for (int i = array.Length - 1; i >= 1; i--)
+            {
+                int j = random.Next(i + 1);
+                // обменять значения data[j] и data[i]
+                var temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+            }
+        }
+
     }
 }
