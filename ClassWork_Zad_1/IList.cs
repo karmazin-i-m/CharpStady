da5d3569b8c23ee5ccb80dfc292e164a71db4050
karmazin.i.m <karmazin.i.m@gmail.com>
2019-08-11 08:50:13 +0300

    Add project files.

---
+++ ClassWork_Zad_1/IList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork_Zad_1
+{
+    interface IList<T>
+    {
+        void Add(T item); // Добавить элемент в коллекцию.
+        void Clear(); // Очистить коллекцию. МАССИВ НУЖНО ПЕРЕСОЗДАТЬ
+        bool Contains(T item); // Возвращает True - если элемент есть в коллекции. False - если нету.
+        void Resize(int capacity); // Изменить размер коллекции на указанный параметр. Если нужно - урезайте.
+        T[] ToArray(); // Вернуть элементы коллекции в виде массива.
+        bool Remove(T item); // Удаляет элемент из коллекции. Если получилось удалить, то возвращает True, не получилось - False.
+        bool RemoveAt(int index); // Удаляет элемент из коллекции по индексу.
+        int IndexOf(T item); // Возвращает индекс элемента внутреннего массива. Если нет такого элемента, то вернуть -1.
+
+        int Count { get; } // Возвращает количество элементов в коллекции добавленных ПОЛЬЗОВАТЕЛЕМ.
+        int Capacity { get; } // Возвращает количество элементов массива. ВНУТРЕННИЙ ОБЪЕМ НАСТОЯЩИЙ.
+        T this[int index] { get; set; } // Индексатор.
+    }
+}
---
+++ ClassWork_Zad_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork_Zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Random random = new Random(DateTime.Now.Millisecond);
+
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(random.Next(0, 100));
+            }
+            Console.WriteLine(list);
+
+            foreach (int element in list)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
