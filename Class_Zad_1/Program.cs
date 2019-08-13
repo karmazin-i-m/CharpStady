85828a67dcfdc5cd08f83a900e70dcbb97a170b5
karmazin.i.m <karmazin.i.m@gmail.com>
2019-08-13 16:42:13 +0300

    ывфывп

---
+++ Class_Zad_1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Классную работу задание 2 реализовано в ClassWork_Zad_1
+            TestClass test = new TestClass(10);
+
+            foreach (var element in test)
+            {
+                Element elem = (Element)element;
+                Console.WriteLine($"A: {elem.A} B: {elem.B} C: {elem.C}");
+            }
+
+            Console.WriteLine(new string('-', 80));
+
+            foreach (var element in test)
+            {
+                Element elem = (Element)element;
+                Console.WriteLine($"A: {elem.A} B: {elem.B} C: {elem.C}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Element
+    {
+        private int a,b,c;
+
+        public Element(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public int A => a;
+        public int B => b;
+        public int C => c;
+    }
+
+    class TestClass : IEnumerable, IEnumerator
+    {
+        Random random = new Random((int)DateTime.Now.Ticks);
+        Element[] items;
+
+        public TestClass(int amount)
+        {
+            items = new Element[amount];
+
+            for (int i = 0; i < amount; i++)
+            {
+                items[i] = new Element(random.Next(100), random.Next(100), random.Next(100));
+            }
+        }
+
+        Element current = null;
+        private int position = -1;
+
+        public object Current => current;
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            if (position<items.Length)
+            {
+                this.current = items[position];
+                return true;
+            }
+            Reset();
+            return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
---
+++ Class_Zad_3/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Zad_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class InnerElementList<T> : IEnumerator<T>, IEnumerable<T>, IEnumerator
+        where T : 
+    {
+        private const int defaultSize = 4;
+        T[] items = ;
+        private int position = -1;
+        T current = default;
+
+        public InnerElementList(int amount)
+        {
+            items = new T[amount];
+            for (int i = 0; i < amount; i++)
+            {
+                items[i] = new T();
+            }
+        }
+
+       object IEnumerator.Current => current;
+
+        public T Current => current;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            if (position < items.Length)
+            {
+                this.current = items[position];
+                return true;
+            }
+            Reset();
+            return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class OuterElementList<T> : IEnumerator<T>, IEnumerable<T>, IEnumerator
+    {
+        private const int defaultSize = 4;
+        T[] items = new T[defaultSize];
+
+        private int position = -1;
+        T current = default;
+
+        object IEnumerator.Current => current;
+
+        public T Current => current;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            if (position < items.Length)
+            {
+                this.current = items[position];
+                return true;
+            }
+            Reset();
+            return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
