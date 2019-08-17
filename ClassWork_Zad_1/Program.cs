862842320ffe099dfd6cd196bf6d2aff21f930d4
karmazin.i.m <karmazin.i.m@gmail.com>
2019-08-17 13:54:34 +0300

    Первые два задания по работе с операторами

--- ClassWork_Zad_1/Program.cs
+++ ClassWork_Zad_1/Program.cs
@@ -10,6 +10,29 @@ namespace ClassWork_Zad_1
     {
         static void Main(string[] args)
         {
+            TestClass1 testClass1 = new TestClass1();
+            TestClass2 testClass2 = new TestClass2();
+
+            Console.WriteLine(testClass1.GetHashCode());
+            Console.WriteLine(testClass2.GetHashCode());
+
+            Console.ReadKey();
         }
     }
+    class TestClass1
+    {
+        public override int GetHashCode()
+        {
+            return Int32.Parse($"{DateTime.Now.Year}{DateTime.Now.Month}{DateTime.Now.Day}{DateTime.Now.Hour}");
+        }
+    }
+    class TestClass2
+    {
+        public override int GetHashCode()
+        {
+            Random random = new Random((int)DateTime.Now.Ticks);
+            return random.Next(Int32.MinValue, Int32.MaxValue);
+        }
+    }
+
 }
---
+++ ClassWork_Zad_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork_Zad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(12);
+            Circle circle1 = new Circle(1);
+
+            Console.WriteLine(circle.Arial);
+            Console.WriteLine(circle1.Arial);
+
+            circle += circle1;
+
+            Console.WriteLine(circle.Arial);
+            Console.ReadKey();
+        }
+    }
+
+    struct Circle
+    {
+        private double radius;
+        private double arial;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+            this.arial = Math.PI * Math.Pow(radius,2);
+        }
+
+        public double Radius => radius;
+        public double Arial => arial;
+
+        public static Circle operator +(Circle circle1, Circle circle2)
+        {
+            return new Circle(Math.Sqrt(((circle1.arial+circle2.arial) / Math.PI)));
+        }
+    }
+}
